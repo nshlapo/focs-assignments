5e3fad746074d9a4b0f4c3a464af083f777156d8
Nur Shlapobersky <nur.shlapobersky@students.olin.edu>
2016-10-30 13:44:08 -0400

    Finished homework for day 17

--- day17/bft_w_obj_nodes.py
+++ day17/bft_w_obj_nodes.py
@@ -7,7 +7,7 @@ Author : Oliver Steele <oliver.steele@olin.edu>
 Date   : 2016-10-27
 License: MIT License
 """
-
+from __future__ import print_function
 from queue import Queue
 
 
@@ -42,21 +42,26 @@ class Node(object):
 
     def __init__(self, label):
         self.label = label
+        self.distance = 0
 
     def __repr__(self):
         return self.label
 
 
-def bfs(graph, start):
+def bfs(graph, start, span_graph):
     remaining_nodes = Queue()
     visited = set()
 
     def visit(node):
         print(node)
+        span_graph.adjacency_list[node] = []
         visited.add(node)
         for tail in graph.successors(node):
             if tail not in visited:
+                tail.distance = node.distance + 1
                 remaining_nodes.put(tail)
+                span_graph.adjacency_list[node].append(tail)
+
 
     remaining_nodes.put(start)
     while not remaining_nodes.empty():
@@ -68,7 +73,7 @@ def node_and_edge_labels_to_objects(node_labels, edge_labels):
     """Given a list of node labels, and a list of edges of the form (head_label, tail_label),
     create and return a list of Node instances with those labels, and a corresponding list of
     edges whose head and tail are those instances."""
-    
+
     nodes = [Node(label) for label in node_labels]
     find_node = {node.label: node for node in nodes}.get
     edges = [(find_node(h), find_node(t)) for (h, t) in edge_labels]
@@ -78,4 +83,13 @@ node_labels = ['a', 'b', 'c', 'd', 'e']
 edge_labels = [('a', 'b'), ('a', 'c'), ('b', 'd'), ('b', 'e'), ('e', 'a')]
 
 g = Graph(*node_and_edge_labels_to_objects(node_labels, edge_labels))
-bfs(g, g.find_node('a'))
+f = Graph()
+
+bfs(g, g.find_node('a'), f)
+
+# Code to show #1 works
+print (f.adjacency_list)
+
+# Code to show #2 works
+for node in g.adjacency_list.keys():
+    print (str(node) + ':' + str(node.distance))
